15b9c8e221c04acfb00dd786ce06b72111348f97
Md.Rahmatuzzaman <42156247+sojib-bd@users.noreply.github.com>
2024-12-04 12:35:19 -0500

    Create 02_with_variables.sh

---
+++ Automation-tool-scripts/Nmap-Dirsearch-Toolkit/scripts/02_with_variables.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+PATH_TO_DIRSEARCH="/usr/local/bin/dirsearch"
+DOMAIN=$1
+DIRECTORY=${DOMAIN}_recon
+echo "Creating directory $DIRECTORY."
+mkdir $DIRECTORY
+nmap $DOMAIN > $DIRECTORY/nmap
+echo "The result of nmap scan are stored in $DIRECTORY/nmap."
+$PATH_TO_DIRSEARCH -u $DOMAIN -e php --output=$DIRECTORY/dirsearch
+echo "The result of dirsearch scan are stored in $DIRECTORY/dirsearch."
